d84220405d05e82a554815d4422e0c5e83f913fd
Mariangel86 <Mariangel_antequera@hotmail.com>
2023-03-29 19:10:49 -0500

    changes of wednesday

---
+++ src/Componentes/firebase/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCj4KJMTJ7S22vZxxlzJTBVasl7f79bi7I",
+  authDomain: "unforgettable-antequera.firebaseapp.com",
+  projectId: "unforgettable-antequera",
+  storageBucket: "unforgettable-antequera.appspot.com",
+  messagingSenderId: "249232032624",
+  appId: "1:249232032624:web:4ded1605aa77df5892c5b5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
